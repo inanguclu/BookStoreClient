3765381621bf7852b901a84af986db2ff4269efa
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-11-15 01:25:49 +0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RequestModel } from '../models/request.model';
+import { BookModel } from '../models/book.model';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { RequestModel } from '../models/request.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  response: any;
+  books: BookModel[]=[];
   categories: any = [];
   pageNumbers: Number[] = [];
   request: RequestModel = new RequestModel();
@@ -17,33 +18,28 @@ export class HomeComponent {
 
 
   constructor(private http: HttpClient) {
-    //this.getAll();
-    //this.getCategories();
-    this.request.pageSize=0;
-    this.feedData();
-
-
+    this.getCategories();
+    
   }
 
   feedData() {
     this.request.pageSize += 10;
     this.newData = [];
-    for (let i = 0; i < this.request.pageSize; i++) {
-      this.newData.push(i);
-    }
+    this.getAll();
   }
   changeCategory(categoryId: number | null = null) {
     this.request.categoryId = categoryId
-    //this.getAll();
+    this.request.pageSize=0;
+    this.feedData();
+    
 
   }
 
   getAll() {
     this.http
-      .post(`https://localhost:7127/api/Books/GetAll/`, this.request)
+      .post<any>(`https://localhost:7127/api/Books/GetAll/`, this.request)
       .subscribe(res => {
-        this.response = res;
-        
+        this.books = res;
       })
   }
 
@@ -53,12 +49,6 @@ export class HomeComponent {
 
   }
 
-  setPageNumber() {
-    this.pageNumbers = [];
-    for (let i = 0; i < this.response.totalPageCount; i++) {
-      this.pageNumbers.push(i + 1)
-    }
-
-  }
+  
 
 }
