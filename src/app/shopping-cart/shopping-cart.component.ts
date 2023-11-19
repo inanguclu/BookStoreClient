1c892915ca5af499bb3761e4928e303d14deb81a
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-11-20 02:18:49 +0300

    .

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ShoppingCartComponent {
 
+
+  constructor(
+    public shopping:ShoppingCartService
+  ){
+    
+  }
 }
