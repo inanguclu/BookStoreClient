4f99c431f5f973d3c49fca99b09ef13072a664a7
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-01-04 18:57:52 +0300

    .

--- src/app/directives/zoom.directive.ts
+++ src/app/directives/zoom.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[zoom]'
@@ -7,14 +7,18 @@ export class ZoomDirective {
   width : string = "";
 
   constructor(
-    private el:ElementRef<HTMLDivElement>
+    private el:ElementRef,
+    private renderer:Renderer2
   ) { }
 
   @HostListener("mouseenter") mouseenter(){
+    this.renderer.addClass(this.el.nativeElement,'zoom');
 
   }
 
   @HostListener("mouseleave") mouseleave(){
+    this.renderer.removeClass(this.el.nativeElement,'zoom');
+
 
   }
 
--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -10,6 +10,7 @@ export class BookModel{
     isDeleted:boolean=false;
     isbn:string="";
     createAt:string="";
+    categories:string[]=[];
     
 }
 
