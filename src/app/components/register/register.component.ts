dea28dd3a9b30c4f4824c3564ef60f22f76b4030
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-01-13 02:20:57 +0300

    add signup

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SwalService } from 'src/app/services/swal.service';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +12,23 @@ import { Router } from '@angular/router';
 export class RegisterComponent {
   constructor(
     private http:HttpClient,
+    private swal:SwalService,
     private router:Router
   ){}
 
   signUp(form:NgForm){
+
+    if(form.valid){
+      this.http.post("",{
+        name:form.controls["name"].value,
+        lastname:form.controls["lastname"].value,
+        username:form.controls["username"].value,
+        email:form.controls["email"].value,
+        password:form.controls["password"].value,
+      }).subscribe(res=>{
+        this.router.navigateByUrl("/login");
+      })
+    }
 
   }
 
