f93c551bc33700dd43277274adf4608c41079345
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-02-08 01:31:57 +0300

    .

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { SwalService } from './swal.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
 
-  constructor(private swal:SwalService) { }
+  constructor(
+    private swal:SwalService,
+    private translate:TranslateService
+    ) { }
 
 
   errorHandler(err:HttpErrorResponse){
     if(err.status==400){
       this.swal.callToast(err.error.message,"error");
+    }else if(err.status==0){
+      let errorMessage= "";
+      this.translate.get("apiNotAvailable").subscribe(res=>{
+        errorMessage=res;
+
+      });
+      this.swal.callToast(errorMessage,"error")
     }
   }
 }
