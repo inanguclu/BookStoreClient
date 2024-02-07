b06f10bd30f2785ffa7286d7e38dd7ab3e8ceeed
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-02-08 01:26:01 +0300

    .

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { SwalService } from './swal.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
 
-  constructor() { }
+  constructor(private swal:SwalService) { }
+
+
+  errorHandler(err:HttpErrorResponse){
+    if(err.status==400){
+      this.swal.callToast(err.error.message,"error");
+    }
+  }
 }
