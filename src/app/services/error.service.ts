6e1d47e703ab9f188b9d82584650a77600f94ee7
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-02-09 17:55:03 +0300

    error service ts

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -9,19 +9,28 @@ import { TranslateService } from '@ngx-translate/core';
 export class ErrorService {
 
   constructor(
-    private swal:SwalService,
-    private translate:TranslateService
-    ) { }
+    private swal: SwalService,
+    private translate: TranslateService
+  ) { }
 
 
-  errorHandler(err:HttpErrorResponse){
-    if(err.status==400){
-      this.swal.callToast(err.error.message,"error");
-    }else if(err.status==0){
-      this.translate.get("apiNotAvailable").subscribe(res=>{
-        this.swal.callToast(res,"error")
+  errorHandler(err: HttpErrorResponse) {
+    if (err.status == 400) {
+      this.swal.callToast(err.error.message, "error");
+      
+    } else if (err.status == 0) {
+      this.translate.get("apiNotAvailable").subscribe(res => {
+        this.swal.callToast(res, "error")
       });
-
     }
+    else if (err.status == 404) {
+      this.translate.get("apiNotFound").subscribe(res => {
+        this.swal.callToast(res, "error")
+      });
+    }
+    else if (err.status == 500) {
+      this.swal.callToast(err.error, "error")
+    }
+
   }
 }
