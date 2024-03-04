c9fb7aeb3da40513a3994f3692ff01673e39a583
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-03-04 03:59:56 +0300

    .

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -67,7 +67,6 @@ export class HomeComponent {
           this.shopping.getAllShoppingCarts();
           this.translate.get("addBookInShoppingCartIsSuccessful").subscribe(res => {
             this.swal.callToast(res);
-            book.quantity-=1;
           });
         },
         error: (err: HttpErrorResponse) => {
